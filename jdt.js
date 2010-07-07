e71156140fb56cc0351f3eddbf7a43b7a2b4ad8f
Yaroslav Lazor <yaroslav.lazor@railsware.com>
2010-07-08 00:53:01 +0300

    - added markers - made a helper class which has all the functionality from jquery and beyound - removed usage of each - made process functon handle json and fixed a few edge cases - added tests - added more test cases

--- jdt.js
+++ jdt.js
@@ -1,92 +1,96 @@
 var JDT = {
-	jquery_defined : function() {
-		return typeof jQuery != 'undefined';
-	},
+	LIST_CLASS_MARKER 		: 'list', 
+	ITEM_CLASS_MARKER 		: 'item', 
+	DELIMITER_CLASS_MARKER 	: 'delimiter', 
+	VALUE_CLASS_MARKER 		: 'value', 
 
 	process : function(data, context) {
-		if (!this.jquery_defined()) { return; };
-		if ( typeof context == 'undefined' ) { context = document.body; };
-		this.process_map(data, context);
-	},
+		if (!this.helper.dependencies_present()) { return; };
+		
+		if ( context === undefined ) { context = document.body; } // no context - use top
+		else if ( typeof data == "string" ) { data = this.helper.parseJSON( data ) } // if it's a string - parse as JSON
+		else if ( context.length != undefined ) { context = context[0]; }; // extract context as array to be the first element
+		
 
-	process_map: function (data, parent) {
-		console.log('process_map', data, parent)
-		var wrapper = this;
-		jQuery.each(data, function(key, value){
-			wrapper.process_element(value, wrapper.find_closest_child(parent, key));
-		})	
-	},
-
-
-	process_array: function (array, parent) {
-		var list_object = this.find_closest_child(parent, 'list');
-		// list class is not defined. Use parent as list class
-		if ( typeof list_object == 'undefined') { list_object = parent }
-
-		var item_templates = this.find_closest_childen(list_object, 'item');
-		if ( item_templates.length < 1 ) {
-			console.log("ERROR: object", parent, "doesn't contains an element with css class 'item'")
-			throw "sopping execution, check console.log"
-		}
-		var item_template = item_templates[0];
-		var item_parent = item_template.parentNode;
-		jQuery(item_templates).remove();
-
-		var delimiter = jQuery(item_parent).find('>.delimiter');
-		delimiter.remove();	
-
-		var wrapper = this;
-		jQuery.each(array, function(index, value){
-			var item = jQuery(item_template).clone()[0];
-			 wrapper.process_element(value, item);
-			if (index > 0 ) { jQuery(item_parent).append( jQuery(delimiter).clone()); };
-			jQuery(item_parent).append(item);
-		})	
-
+		this.process_element(data, context);
 	},
 
 	process_element: function (value, parent) {
-		if ( typeof parent == 'undefined' ) { return; } // nothing to do. We don't have an element for the json
-	
 		if ( value instanceof Array) { this.process_array(value, parent); }
 		else if ( value instanceof Object) { this.process_map(value, parent); }
-		else { this.process_value(value, parent); }
+		else { this.process_value(value, parent); };
+	},
+
+	process_map: function (data, parent) {
+		this.helper.log('process_map', data, parent);
+		for ( key in data ) {
+			var context = this.find_closest_child(parent, key);
+			if ( context !== undefined ) { this.process_element(data[key], context); };
+		}
 	},
 
 
 	process_value: function (value, parent) {
-		var value_object = jQuery(parent).find('.value');
-		var text_container = value_object.length>0 ? value_object[0] : parent;
-		jQuery(text_container).text(value);
+		var value_object = this.find_closest_child(parent, this.VALUE_CLASS_MARKER);
+		var text_container = (value_object === undefined) ? parent : value_object;
+		this.helper.text(text_container, value);
 	},
 
+	process_array: function (array, parent) {
+		var list_object = this.find_closest_child(parent, this.LIST_CLASS_MARKER);
+		// list class is not defined. Use parent as list class
+		if ( list_object === undefined) { list_object = parent };
 
+		var item_templates = this.find_closest_childen(list_object, this.ITEM_CLASS_MARKER);
+		if ( item_templates.length < 1 ) {
+			this.helper.error("ERROR: object", parent, "doesn't contains an element with css class '"+this.ITEM_CLASS_MARKER+"'");
+		};
+		var item_template = item_templates[0];
+		var item_parent = item_template.parentNode;
+		this.helper.remove(item_templates);
+
+		var delimiter = this.helper.find(item_parent, '>.'+this.DELIMITER_CLASS_MARKER);
+		delimiter.remove();	
+
+		for ( var index=0; index < array.length; index ++) {
+			var item = this.helper.clone(item_template);
+			this.process_element(array[index], item);
+			if (index > 0 ) { this.helper.append(item_parent,  this.helper.clone(delimiter)); };
+			this.helper.append(item_parent, item);
+		}
+	},
+	
 	find_closest_childen: function (parent, key) {
 		// if parent has the class itself, let's return it right away
-		if ( jQuery(parent).hasClass(key)) { return [parent]; };
+		if ( this.helper.hasClass(parent, key)) { return [parent]; };
 
-		var children = jQuery(parent).find('.'+key)
+		var children = this.helper.find(parent, '.'+key);
 
 		// has one child ?
 		if (children.length <= 1) { return children; };
 	
 		// let's find only closest depth children is to the parent
 		var closest = [];
-		var iterated_parent_nodes = jQuery(children); // clone
+
+		var iterated_parent_nodes = []; // storage
+		var length = children.length;
+		for ( var index=0; index < length; index++) {iterated_parent_nodes[index]=children[index];};
+		
 		while ( closest.length == 0 ) {
 
 			// every iteration will go up 1 parentNode and search for children with parentNode == parent
-			jQuery( iterated_parent_nodes ).each( function( index, previous_parent_node ) {
-				var current_parent_node = previous_parent_node.parentNode;
+			for ( var index=0; index < length; index++) {
+				var current_parent_node = iterated_parent_nodes[index].parentNode;
+				this.helper.log(' key', key, 'parent', parent, 'current_parent_node', current_parent_node);
 
-				if ( current_parent_node == parent ) { closest[closest.length++] = children[index]; }
+				if ( current_parent_node == parent ) { closest[closest.length++] = children[index]; };
 			
 				// set next iteration of parent_node for element
 				iterated_parent_nodes[index] = current_parent_node;
-			} )
+			}
 
 		}
-	
+
 		return closest;
 	}, 
 	
@@ -94,10 +98,27 @@ var JDT = {
 		var children = this.find_closest_childen(parent, key);
 
 		if ( children.length > 1 ) {
-			console.log('ERROR: you have more than one element for parent: ', parent, ' with class: ', key, '. Consider refactoring your HTML or JSON structure');
-			throw "stopping execution, check console.log"
+			this.helper.error('you have more than one element for parent: ', parent, ' with class: ', key, '. Consider refactoring your HTML or JSON structure');
 		}
 
 		return children[0];
+	},
+	
+	helper: {
+		ERROR : true,
+		DEBUG : false,
+		
+		dependencies_present: function() { return jQuery !== undefined; },
+		parseJSON: jQuery.parseJSON,
+		each     : function (object, arg) { return jQuery.each(object, arg) }, 
+		remove   : function (object) { return jQuery(object).remove() },
+		clone    : function (object) { return jQuery(object).clone()[0] },
+		text     : function (object, arg) { return jQuery(object).text(arg) },
+		find     : function (object, arg) { return jQuery(object).find(arg) },
+		append   : function (object, arg) { return jQuery(object).append(arg) },
+		hasClass : function (object, arg) { return jQuery(object).hasClass(arg) },
+		log      : function (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) { if (this.DEBUG) { console.log.apply(this, arguments); } },
+		error    : function (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) { if (this.ERROR) { console.log.apply(this, arguments); throw "stopping execution, check console.log" } }
 	}
+
 }
